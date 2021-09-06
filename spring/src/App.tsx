cd8a8b53f2106a72d6c47a447d52a53f82368d37
flaviozno <flavio_contato@outlook.com>
2021-09-06 09:47:54 -0300

    ✅ dia 1 (estatico)

--- spring/src/App.tsx
+++ spring/src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
+import Navbar from './components/Navbar/index'
+import Footer from './components/Footer/index'
+import DataTable from './components/DataTable/index'
+import BarChart from './components/BarChart/index'
+import Donut from './components/DonutChart/index'
+
 function App() {
   return (
-    <div className="App">
+    <div className="text-primary">
+        <Navbar />
+        <div className="row px-3">
+            <div className="col-sm-6">
+                <h5>Taxa de sucesso %</h5>
+                <BarChart />
+            </div>
+            <div className="col-sm-6">
+                <h5>Todas as vendas</h5>
+                <Donut />
+            </div>
+        </div>
+        <DataTable />
+        <Footer />
     </div>
   );
 }
